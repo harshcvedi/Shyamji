f6fc50031b1afbfc5ea290c58eb2c24f0b426322
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 15:36:08 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,8 @@
+public static void printSubString(String str) {
+
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				System.out.println(str.substring(i, j));
+			}
+		}
+	}
